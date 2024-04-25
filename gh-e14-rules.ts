778f2815fb38f6b1364cba3f360ab32c73827958
Pablo Bangueses <pablobngs@gmail.com>
2024-04-25 18:04:26 -0300

    Test release

---
+++ gh-e14-rules.ts
@@ -0,0 +1,14 @@
+// import getArgs from './src/getArgs.ts';
+// import printHelp from './src/printHelp.ts';
+// import runCommand from './src/runCommand.ts';
+
+// const { command, help, ...args } = getArgs();
+
+// if (!command || help) {
+//   printHelp(command);
+//   Deno.exit();
+// }
+
+// runCommand(command, args);
+
+console.log('Hello World');
--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
-echo "TODO implement this script."
-echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
-exit 1
+
+platforms=(
+  "darwin-amd64,x86_64-apple-darwin"
+  "darwin-arm64,aarch64-apple-darwin"
+  "linux-amd64,x86_64-unknown-linux-gnu"
+  "linux-arm64,aarch64-unknown-linux-gnu"
+  "windows-amd64,x86_64-pc-windows-msvc"
+)
+
+mkdir -p dist
+
+for p in "${platforms[@]}"; do
+  filename="${p%,*}"
+  target="${p#*,}"
+  os="${filename%-*}"
+
+  ext=""
+  if [ "$os" = "windows" ]; then
+    ext=".exe"
+  fi
+
+  deno compile --allow-read --allow-net --allow-run \
+    --output dist/${filename}${ext} \
+    --target ${target} \
+    gh-e14-rules.ts
+done
